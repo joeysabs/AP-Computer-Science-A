14a14eae56b9969c77e149292c0e5253bf160025
joeysabs <joeysabs123@gmail.com>
2020-01-18 15:13:10 -0500

    Add files via upload

---
+++ Mod01/1.05 Hello World/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class HelloWorld here.
+
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+        public static void main(String[] args) 
+        {
+            System.out.print("Hello, Virtual World!");
+            System.out.println("3.14");
+            System.out.println("It is a great day for programming.");
+        }
+}
---
+++ Mod01/1.05 Student Info/StudentInfoCard.java
@@ -0,0 +1,31 @@
+
+/**
+ * Printing student info
+ *
+ * @author (Joey Sabs)
+ * @version (9/28/18)
+ */
+public class StudentInfoCard
+{
+        public static void main(String[] args)
+        {
+            System.out.println("        STUDENT INFORMATION CARD");
+            System.out.print("Name: Joey Sabs       ");
+            System.out.println("            School: Lacey Township High School");
+            System.out.print("Birthday: 22 February 2002    ");
+            System.out.println("    City: Lanoka Harbor");
+            System.out.print("Age: 16      ");
+            System.out.println("                     Grade: 11");
+            System.out.println();
+            System.out.println("Cell Phone: (609)-618-5914, Available all of the time");
+            System.out.println("Email: joeysabs123@gmail.com, Available all of the time");
+            System.out.println();
+            System.out.println("Parents Contact Info");
+            System.out.println("Email: jnsab@comcast.net");
+            System.out.println("Cell Phone: (609)-619-0301");
+            System.out.println();
+            System.out.println("Current Math Course: AP Calc AB"); 
+            System.out.println("Programming Experience: I know a great amount about programming and the way it works. I have spent many hours online studying it.");
+            System.out.print("Reason for Taking Course: I am taking this couse because I love math and science. Also, I plan on majoring in computer science in college.");
+        }
+}        
